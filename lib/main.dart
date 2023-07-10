a7e9b3c705391c2c08df94f9b77a366e1605cc00
dalejandro13 <dalejandro131086@gmail.com>
2023-07-10 17:33:54 -0500

    documentation
    I add in main.dart the documentation of application

--- lib/main.dart
+++ lib/main.dart
@@ -107,3 +107,43 @@ class _UdpCommunicationState extends State<UdpCommunication> with WidgetsBinding
     );
   }
 }
+
+/*
+DOCUMENTACION:
+
+1) CircularButton (Iniciar):
+Cuando presiono el boton verde de la aplicacion, envio la trama: 0x55(U) para consultar el estado de las variables en el ESP.
+EL ESP me regresa los siguientes datos: "U:31-32-33-34". 
+
+En este caso la informacion se interpreta de la siguiente forma:
+Cuando ozono esta activado (31) se habilita el switchButton "Ozono".
+Cuando compresor esta activado (-32) se habilita el switchButton "Compresor".
+Cuando activacion esta activado (-33) se habilita el switchButton "Activar".
+Cuando ambientador esta activado (-34) se habilita el switchButton "Ambientador".
+
+Luego se envia el comando 0x41(A) para que el ESP haga sonar el tono de inicio.
+
+Luego desde el ESP se envia constantemente los valores de ozono de la siguiente forma: 
+"O3 -> ppm: X.XXXX - ppb: YYYY"
+
+Despues se activa el timer el cual activara automaticamente el ozono y el compresor pasado 5 minutos.
+
+2) Boton "limpiar pantalla":
+Este boton cuando se presiona no envia comandos al ESP, solo limpia la pantalla de la aplicacion.
+
+3) Boton "desconectar todo":
+Cuando se presiona este boton se envia el comando 0x39(9) para que el ESP apague todo el sistema.
+Pero antes que de que se apague el ESP, el micro envia una ultima respuesta, envia una 'D:' para que la aplicacion
+se desconecte del protocolo UDP y la interfaz se "reinicie".
+
+4) SwitchButton:
+ozono:       si lo activo se envia el comando 0x31(1) al ESP. Cuando se desactiva se envia el 0x35(5) al ESP
+compresor:   si lo activo se envia el comando 0x32(2) al ESP. Cuando se desactiva se envia el 0x36(6) al ESP
+activacion:  si lo activo se envia el comando 0x33(3) al ESP. Cuando se desactiva se envia el 0x37(7) al ESP
+ambientador: si lo activo se envia el comando 0x34(4) al ESP. Cuando se desactiva se envia el 0x38(8) al ESP
+
+5) Boton desplazar:
+Ese boton, el cual es un checkBox, solo habilita ó deshabilita el autoScroll, no envia comandos para 
+interactuar con el ESP.
+
+*/
